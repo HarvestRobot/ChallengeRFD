cbd4bdc55c98681ce7139aefc52bfa6be94653db
Geadalu <luciapiedrabuena1@gmail.com>
2022-05-01 20:47:12 +0200

    Database creation dump I also put grids to the Excels.

--- auxData/database_dump/postgres_public_packageassignments.sql
+++ auxData/database_dump/postgres_public_packageassignments.sql
@@ -1,3 +1,22 @@
+create table packageassignments
+(
+    idpackage integer not null
+        constraint packageassignments_pk_2
+            unique,
+    idvehicle varchar not null,
+    idrow     serial
+        constraint packageassignments_pk
+            primary key
+);
+
+alter table packageassignments
+    owner to postgres;
+
+create unique index packageassignments_idrow_uindex
+    on packageassignments (idrow);
+
 INSERT INTO public.packageassignments (idpackage, idvehicle, idrow) VALUES (2, 'A2', 2);
 INSERT INTO public.packageassignments (idpackage, idvehicle, idrow) VALUES (5, 'CR2564', 10);
 INSERT INTO public.packageassignments (idpackage, idvehicle, idrow) VALUES (8, 'CR2564', 27);
+INSERT INTO public.packageassignments (idpackage, idvehicle, idrow) VALUES (11, 'CR2564', 44);
+INSERT INTO public.packageassignments (idpackage, idvehicle, idrow) VALUES (1, 'CR25434', 56);
--- auxData/database_dump/postgres_public_packagedata.sql
+++ auxData/database_dump/postgres_public_packagedata.sql
@@ -1,3 +1,21 @@
+create table packagedata
+(
+    idpackage integer   not null,
+    idvehicle varchar   not null,
+    latitude  varchar   not null,
+    longitude varchar   not null,
+    timestamp timestamp not null,
+    location  varchar   not null,
+    idrow     serial
+        constraint packagedata_pk
+            primary key,
+    constraint packagedata_pk_2
+        unique (idpackage, timestamp)
+);
+
+alter table packagedata
+    owner to postgres;
+
 INSERT INTO public.packagedata (idpackage, idvehicle, latitude, longitude, timestamp, location, idrow) VALUES (1, 'A1', '6.5652445', '35.65324565', '2022-04-29 00:24:02.000000', 'Madrid', 2);
 INSERT INTO public.packagedata (idpackage, idvehicle, latitude, longitude, timestamp, location, idrow) VALUES (2, 'A2', '5.653214', '2.3256523', '2022-07-30 00:24:41.000000', 'Valdepeñas', 4);
 INSERT INTO public.packagedata (idpackage, idvehicle, latitude, longitude, timestamp, location, idrow) VALUES (1, 'A1', '6.5656565', '35.65656565', '2022-03-28 00:23:39.000000', 'Madrid', 1);
@@ -9,3 +27,9 @@ INSERT INTO public.packagedata (idpackage, idvehicle, latitude, longitude, times
 INSERT INTO public.packagedata (idpackage, idvehicle, latitude, longitude, timestamp, location, idrow) VALUES (99, 'TESTVEHICLE', 'TESTLATITUDE', 'TESTLONGITUDE', '2022-04-29 20:26:57.748875', 'TESTLOCATION', 33);
 INSERT INTO public.packagedata (idpackage, idvehicle, latitude, longitude, timestamp, location, idrow) VALUES (99, 'TESTVEHICLE', 'TESTLATITUDE', 'TESTLONGITUDE', '2022-04-29 20:27:20.759455', 'TESTLOCATION', 34);
 INSERT INTO public.packagedata (idpackage, idvehicle, latitude, longitude, timestamp, location, idrow) VALUES (1, 'CR2564', '40.48725603806988', '-3.6789361594249685', '2022-04-29 18:44:17.186234', 'Madrid', 37);
+INSERT INTO public.packagedata (idpackage, idvehicle, latitude, longitude, timestamp, location, idrow) VALUES (12, 'CR2564', '40.48725603806988', '-3.6789361594249685', '2022-04-29 20:50:24.908000', 'Puertollano', 38);
+INSERT INTO public.packagedata (idpackage, idvehicle, latitude, longitude, timestamp, location, idrow) VALUES (12, 'CR2564', '40.48725603806988', '-3.6789361594249685', '2022-04-29 20:52:40.765809', 'Puertollano', 39);
+INSERT INTO public.packagedata (idpackage, idvehicle, latitude, longitude, timestamp, location, idrow) VALUES (12, 'CR2564', '40.48725603806988', '-3.6789361594249685', '2022-04-29 20:52:49.431827', 'Puertollano', 40);
+INSERT INTO public.packagedata (idpackage, idvehicle, latitude, longitude, timestamp, location, idrow) VALUES (12, 'CR2564', '40.48725603806988', '-3.6789361594249685', '2022-04-29 22:40:40.593853', 'Puertollano', 48);
+INSERT INTO public.packagedata (idpackage, idvehicle, latitude, longitude, timestamp, location, idrow) VALUES (12, 'CR2564', '40.48725603806988', '-3.6789361594249685', '2022-04-29 22:42:43.432118', 'Puertollano', 50);
+INSERT INTO public.packagedata (idpackage, idvehicle, latitude, longitude, timestamp, location, idrow) VALUES (1, 'CR2564', '40.48725603806988', '-3.6789361594249685', '2022-04-29 22:47:35.392602', 'Madrid', 53);
